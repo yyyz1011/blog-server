aa74fccf1af26eba90ca9f5df77ea999419bcdec
yzcode <1540032876@qq.com>
2022-03-26 00:17:56 +0800

    fix: update tip

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,13 +8,16 @@ const router = new Router({
   prefix: config.api_prefix,
 });
 
+// 用户接口 token
 router.get("/user/getUserInfo", UserController.getUserInfo);
 
+// Test get请求测试
 router.get(
   "/test/getTestDemoByGet",
   checkToken,
   TestController.getTestDemoByGet
 );
+// Test post请求测试
 router.post(
   "/test/getTestDemoByPost",
   checkToken,
