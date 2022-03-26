30452142ae44cf2293567b9a0a2dfb5cef3ccd8a
yzcode <1540032876@qq.com>
2022-03-26 15:25:32 +0800

    feat: add mongoose config

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,21 @@ import * as Koa from "koa";
 import * as KoaBody from "koa-body";
 import * as KoaStatic from "koa-static";
 import * as path from "path";
+import * as mongoose from "mongoose";
 import config from "./config";
 import router from "./routes";
 import catchError from "./middleware/catchError";
+
+// const mongooseUrl = `mongodb://${config.db.user}:${config.db.password}@${config.db.host}:${config.db.port}/${config.db.database}`;
+// console.log(mongooseUrl)
+// mongoose.connect(mongooseUrl);
+// const db = mongoose.connection;
+// db.on("error", () => {
+//   console.log("数据库连接出错!");
+// });
+// db.once("open", () => {
+//   console.log("数据库连接成功！");
+// });
 
 const App = new Koa();
 
